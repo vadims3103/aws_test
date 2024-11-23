9b3ad1c78c4315e48f3d06cdf5a0f3e1b630ce18
vadims3103 <vadims3103@yahoo.com>
2024-11-24 00:22:32 +0200

    created models for blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+
+    def __str__(self):
+        return f"{self.title}"
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from .models import Blog
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
 def main(request):
-    return render(request, 'main.html')
+    blogs = Blog.objects.all()
+    template = loader.get_template('main.html')
+    context = {
+        'blogs': blogs
+    }
+    return HttpResponse(template.render(context, request))
